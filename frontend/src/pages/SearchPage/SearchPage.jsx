c511466e26ebd8303987ac54ba97b796afeeb0fd
Victoria Probst <victoria.probst13@gmail.com>
2022-07-14 16:18:07 -0400

    Started Mapping Functionality to Display Search Results

---
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const SearchPage = (props) => {
+    console.log(props.searchResults);
+    
+
+    return (
+        <><div>
+            <h1>
+                <p>Results</p>
+            </h1>
+        </div><div>
+            {props.searchResults.map((item)=>{
+                return(
+                    <ul>
+                    <li> {item.items.volumeInfo.title}</li>
+                    </ul>
+  
+                );
+                
+         
+            })}
+        
+            </div></>
+       
+    );
+
+}
+export default SearchPage;
